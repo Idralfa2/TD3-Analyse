7bde2b0702fd69ab984d50ed6cef2e9340e58791
Flavien Grihard IUT <flappybirdy3011@gmail.com>
2025-09-16 20:11:09 +0200

    Creation Classe Hello

---
+++ trace/src/main/java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static String hello(){
+        return "Hello, World!";
+    }
+
+    public static String hello(String nom){
+        return "Hello" + nom + " !";
+    }
+}
